f908eef571dd8a73a4fc55ec8efdfd416d6da7f1
Mickelgt <mickelgerges1@gmail.com>
2025-03-02 15:11:04 +0000

    Update passwordreset.py

--- Gym/passwordreset.py
+++ Gym/passwordreset.py
@@ -6,7 +6,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="MickelUWE",
-    password="Jram1212", 
+    password="# Add your own password", 
     database="Gym"
     )
 cursor = conn.cursor()
